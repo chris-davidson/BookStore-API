b7baf49607f19cdccc2de09b01d4d334393fec86
Chris Davidson <chris.davidson@crossmatch.com>
2020-10-07 16:31:17 -0400

    Added CRUD operations for Book, fixed the Book API with DTO issues and added a starting point for Blazored Toast  notifications.

--- BookStore-UI/Startup.cs
+++ BookStore-UI/Startup.cs
@@ -15,6 +15,7 @@ using Blazored.LocalStorage;
 using System.IdentityModel.Tokens.Jwt;
 using BookStore_UI.Providers;
 using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.Toast;
 
 namespace BookStore_UI
 {
@@ -34,6 +35,7 @@ namespace BookStore_UI
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddBlazoredLocalStorage();
+            services.AddBlazoredToast();
             services.AddHttpClient();
             services.AddScoped<ApiAuthenticationStateProvider>();
             services.AddScoped<AuthenticationStateProvider>(p => 
@@ -41,6 +43,8 @@ namespace BookStore_UI
             services.AddScoped<JwtSecurityTokenHandler>();
             services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
             services.AddTransient<IAuthorRepository, AuthorRepository>();
+            services.AddTransient<IBookRepository, BookRepository>();
+            services.AddTransient<IFileUpload, FileUpload>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
